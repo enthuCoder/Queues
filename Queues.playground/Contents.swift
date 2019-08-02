187d89da1e6bc94ef69fca825cc03155f2c103c7
Dilgir Siddiqui <dsiddiqui@apple.com>
2019-08-02 04:17:17 -0700

    Added stubs for Queue implementation

--- Queues.playground/Contents.swift
+++ Queues.playground/Contents.swift
@@ -1,3 +1,2 @@
 import Cocoa
 
-var str = "Hello, playground"
--- Queues.playground/Sources/Queues.swift
+++ Queues.playground/Sources/Queues.swift
@@ -1,2 +1,60 @@
 import Foundation
 
+// Queue is a FIFO DS
+// Enqueuing and Dequeuing happens at different ends
+// Enqueue and Dequeue are O(1) operations
+
+// ------------------------------------------------------------
+// MARK: Implement Queue using an Array as the base storage
+// ------------------------------------------------------------
+
+public struct Queue<Type: Comparable> {
+
+    public var storage = [Type]()
+    
+    public init() { }
+    
+    public var minimum: Type?
+
+}
+
+// ------------------------------------------------------------
+// MARK: Implement Queue Helper functions
+// ------------------------------------------------------------
+
+extension Queue {
+    
+    public var count: Int {
+        return storage.count
+    }
+    
+    public var isEmpty: Bool {
+        return storage.isEmpty
+    }
+    
+    public func front() -> Type? {
+        return nil
+    }
+    
+    public func last() -> Type? {
+        return nil
+    }
+    
+    public func enqueue(_ newValue: Type) {
+        
+    }
+    
+    public func dequeue() {
+        
+    }
+}
+
+// ------------------------------------------------------------
+// MARK: Queue representation
+// ------------------------------------------------------------
+extension Queue: CustomStringConvertible {
+    
+    public var description: String {
+        return ""
+    }
+}
